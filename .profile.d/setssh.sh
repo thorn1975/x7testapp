3523df1b3835957dd1b6d83813400029f4390373
thorn1975 <anareli507@etzdnetx.com>
2018-01-31 15:32:33 +0700

    Fix

---
+++ .profile.d/setssh.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+echo "root:$SSH_PASS" | chpasswd
+sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd
